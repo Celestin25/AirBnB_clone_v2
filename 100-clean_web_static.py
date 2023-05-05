e27d118b3f9aa54ba22c68d8ee4848dd43a9c2c6
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2023-05-05 20:29:32 +0200

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -15,6 +15,7 @@ def do_clean(number=0):
     etc.
     """
     number = 1 if int(number) == 0 else int(number)
+    
 
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
