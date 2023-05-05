312aeee2281d439dec82fb3386a827e6e657439f
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2023-05-05 20:29:03 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,6 +13,7 @@ def do_pack():
     """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
+        
         if isdir("versions") is False:
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
